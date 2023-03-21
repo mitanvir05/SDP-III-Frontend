2251a58f082e781aece77e62d4065c2e142a50ba
mitanvir05 <tanvir.cse05@gmail.com>
2023-03-21 21:49:33 +0600

    footer added

--- src/components/Home/Info/Info.jsx
+++ src/components/Home/Info/Info.jsx
@@ -2,28 +2,31 @@ import React from 'react';
 
 //  internal imports
 import InfoCard from './InfoCard';
-import Clock from '../../../assets/icons/clock.svg';
-import Marker from '../../../assets/icons/marker.svg';
-import Phone from '../../../assets/icons/phone.svg';
+import clock from '../../../assets/icons/clock.svg';
+import marker from '../../../assets/icons/marker.svg';
+import phone from '../../../assets/icons/phone.svg';
 
 const Info = () => {
   return (
-    <div className="grid grid-cols-1 px-6 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
       <InfoCard
+        opening="8 a.m. to 6 p.m."
         cardTitle="Opening Hours"
         bgClass="bg-gradient-to-r from-secondary to-primary"
-        img={Clock}
-      />
+        img={clock}
+      ></InfoCard>
       <InfoCard
-        cardTitle="Visit our location"
-        bgClass="bg-accent"
-        img={Marker}
-      />
+        location="Mirpur 13"
+        cardTitle="Our Locations"
+        bgClass="bg-neutral"
+        img={marker}
+      ></InfoCard>
       <InfoCard
-        cardTitle="Contact us now"
+        contact="+8801700955009"
+        cardTitle="Contact Us"
         bgClass="bg-gradient-to-r from-secondary to-primary"
-        img={Phone}
-      />
+        img={phone}
+      ></InfoCard>
     </div>
   );
 };
--- src/components/Home/Info/InfoCard.jsx
+++ src/components/Home/Info/InfoCard.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const InfoCard = ({ img, cardTitle, bgClass }) => {
+const InfoCard = ({ img, opening, location, contact, cardTitle, bgClass }) => {
   return (
-    <div className={`card p-6 ${bgClass} lg:card-side shadow-xl`}>
-      <figure>
+    <div className={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+      <figure className="pl-5 pt-5">
         <img src={img} alt="Album" />
       </figure>
       <div className="card-body text-white">
         <h2 className="card-title">{cardTitle}</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
+        <p>{opening}</p>
+        <p>{location}</p>
+        <p>{contact}</p>
       </div>
     </div>
   );
